56d9089c818af1c5cb191932664e0440cec110cc
jadev1 <48669661+jadev1@users.noreply.github.com>
2022-11-25 19:53:42 +0100

    Changed CUSTOM_MACHINE_NAMSE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -136,7 +136,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "X5SA BTT-SKR-PRO-1.1"
+#define CUSTOM_MACHINE_NAME "X5SA - SKR-PRO-1.1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
